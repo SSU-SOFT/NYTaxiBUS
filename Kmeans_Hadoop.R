846917bc7d10514890c274714c4596e0fda2c7a3
Rude_zoo <zoo5871@naver.com>
2021-12-15 20:30:53 +0900

    Create Kmeans_Hadoop.R

---
+++ Kmeans_Hadoop.R
@@ -0,0 +1,153 @@
+library(rhdfs)
+hdfs.init()
+library(rmr2)
+
+rmr.options(backend = "local")
+
+# geo coding 시각화
+#install.packages('ggplot2')
+#install.packages('ggmap')
+#install.packages("devtools")
+
+
+rm(list=ls())
+rmr.options(backend = "local")
+#rmr.options(backend = "hadoop")
+
+files<-list()
+files[1]<-"./data/taxi/info.csv";
+files[2]<-"./data/taxi/sample_combined1.csv"
+
+mr <- mapreduce(input = files[1], 
+                input.format = make.input.format(
+                  format = "csv", sep=",", stringsAsFactors=F)
+)
+
+res <- from.dfs(mr) 
+ress <- values(res)
+colnames.tmp <- as.character(ress[,1]); 
+class.tmp <- as.character(ress[,2]); 
+colnames <- colnames.tmp[-1]; 
+class <- class.tmp[-1]; class 
+class[c(6,8,9,10)] <- "numeric"
+
+cbind(colnames, class)
+
+input.format <- make.input.format( 
+  format = "csv", sep = ",", 
+  stringsAsFactors = F, 
+  col.names = colnames, colClasses = class)
+files <- files[-1]
+files
+colnames
+
+data<-read.csv("./data/taxi/sample_combined1.csv")
+
+class(data)
+names(data)<-colnames
+data <- subset(data, pickup_latitude >= 40 & pickup_latitude <= 42)
+data <- subset(data, pickup_longitude >= -75 & pickup_longitude <= -73)
+data <- subset(data, dropoff_latitude >= 40 & dropoff_latitude <= 42)
+data <- subset(data, dropoff_longitude >= -75 & dropoff_longitude <= -73)
+
+
+head(data, 10)
+
+# - John F Kenedy 국제공함
+# : 40.649352, -73.793321 (Left Top)
+# : 40.639029, -73.775726 (right Bottom)
+JFK_LT = c(40.649352, -73.793321)
+JFK_RB = c(40.639029, -73.775726)
+JFK <- subset(data, ((JFK_RB[1] <= dropoff_latitude) & (dropoff_latitude <= JFK_LT[1])))
+JFK <- subset(JFK, ((JFK_LT[2] <= dropoff_longitude) & (dropoff_longitude <= JFK_RB[2])))
+
+# - 뉴욕 라과디아 공항
+# : 40.776372, -73.877144 (Left Top)
+# : 40.766438, -73.860201 (Right Bottom)
+#LG_LT = c(40.776372, -73.877144)
+#LG_RB = c(40.766438, -73.860201)
+#LG <- subset(data, ((LG_RB[1] <= dropoff_latitude) & (dropoff_latitude <= LG_LT[1])))
+#LG <- subset(LG, ((LG_LT[2] <= dropoff_longitude) & (dropoff_longitude <= LG_RB[2])))
+
+# - 뉴어크 리버티 국제공항
+# : 40.696027, -74.184740 (Left Top)
+# : 40.687360, -74.176749 (Right Bottom)
+#Newark_LT = c(40.696027, -74.184740)
+#Newark_RB = c(40.687360, -74.176749)
+#Newark <- subset(data, ((Newark_RB[1] <= dropoff_latitude) & (dropoff_latitude <= Newark_LT[1])))
+#Newark <- subset(Newark, ((Newark_LT[2] <= dropoff_longitude) & (dropoff_longitude <= Newark_RB[2])))
+
+dat <- cbind(JFK['pickup_latitude'], JFK['pickup_longitude']); dat
+
+
+dist.fun <- function(C, P){
+  apply(C, 1, function(x) colSums((t(P) - x)^2))
+}
+
+
+kmeans.map<-function(k,v) {
+  nearest<-if(is.null(C)){
+        #print("C in null")
+        #print(num.clusters)
+        sample(1:num.clusters,nrow(v),replace=TRUE)
+      }
+      else {
+        D<-dist.fun(C,v)
+        nearest<-max.col(-D)
+      }
+  #print("map run")
+ 
+  keyval(nearest,v)
+}
+kmeans.reduce<-function(k,v){
+  print(v)
+  keyval(k,t(as.matrix(apply(v,2,mean))))
+} 
+
+
+num.clusters<-10
+Kmeans_mr<-function(P,num.iters=5){
+ 
+  for(i in 1:num.iters){
+    result<-from.dfs(mapreduce(input=P,map=kmeans.map,reduce=kmeans.reduce))
+    #print(result)
+    C<<-result$val
+    #if(nrow(C)<num.clusters){
+    #  C<-rbind(C,matrix(rnorm((num.clusters-nrow(C))*nrow(C)),ncol=nrow(C))%*%C)
+    #}
+  }
+  return(C)
+}
+
+#실행은 여기부터 
+C<-NULL
+num.iters<-10
+centers<-Kmeans_mr(P=to.dfs(dat),num.iters=num.iters)
+centers
+
+
+#원래 데이터 정재
+fit <- kmeans(dat, centers = 10)
+
+fit$cluster
+fit$centers
+fit
+fit_filter <- subset(as.data.frame(fit$centers), ((40 <= pickup_latitude) & (pickup_latitude <= 42)))
+class(fit$centers)
+
+# 시각화
+library(ggplot2)
+library(ggmap)
+
+#center 정제
+
+centers_filtered<- subset(as.data.frame(centers), ((40 <= pickup_latitude) & (pickup_latitude <= 42)))
+centers_filtered
+
+register_google(key='AIzaSyCMaV4yY0ZirrR_dbKmSn74PRPu4O9Q26c')
+map<-get_map(location='Manhatten', zoom=11)
+ggmap(map)+geom_point(data=centers_filtered,aes(x=pickup_longitude,y=pickup_latitude,color='red',alpha=0.3))+geom_point(data=fit_filter,aes(x=pickup_longitude,y=pickup_latitude,alpha=0.3))
+
+fit_filter
+centers_filtered
+
